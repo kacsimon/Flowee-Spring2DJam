e9d27610f4e6192aeed15c5c6e543b815c2d6017
kacsimon <132482344+kacsimon@users.noreply.github.com>
2024-05-18 17:28:35 +0200

    Menu
    - Add Menu
    - Add assets
    - Create ScriptableObjects
    - Create background
    - Make Prefabs

--- Assets/Scripts/FlowerSO.cs
+++ Assets/Scripts/FlowerSO.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 
-public class FlowerSO : MonoBehaviour
+[CreateAssetMenu(fileName = "Flower")]
+public class FlowerSO : ScriptableObject
 {
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
+    public int id;
+    public string flowerName;
+    public string description;
+    public GameObject seedPrefab; //or Transform
+    public GameObject seedlingPrefab;
+    public GameObject prefab;
+    public GameObject fullyGrownPrefab;
+    public Sprite sprite;
 }
---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
